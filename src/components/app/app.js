7d8f01f70dbe206f5682648777d0a6294819add6
x-maria-x <m_khvatova99@mail.ru>
2023-11-02 01:36:49 +0700

    added a search form

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,82 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Input, Tooltip } from 'antd'
+import { CloseOutlined } from '@ant-design/icons'
+import debounce from 'lodash.debounce'
 
-import './app.css'
+import TmdbService from '../../services/tmdb-service'
 import Movies from '../movies/movies'
+import './app.css'
 
-function App() {
-  return (
-    <section className="wrapper">
-      <Movies />
-    </section>
-  )
+export default class App extends Component {
+  state = {
+    movies: [],
+    isLoading: false,
+    error: null,
+    label: '',
+    noResults: false,
+  }
+
+  getMoviesList = debounce(() => {
+    this.setState({ isLoading: true, noResults: false })
+    const moviesService = new TmdbService()
+    const labelSearch = this.state.label
+    moviesService
+      .getMovies(
+        `https://api.themoviedb.org/3/search/movie?query=${labelSearch}&include_adult=false&language=en-US&page=1`
+      )
+      .then((moviesData) => {
+        if (moviesData.length === 0) {
+          this.setState({ isLoading: false, movies: moviesData, noResults: true })
+        } else this.setState({ isLoading: false, movies: moviesData, noResults: false })
+      })
+      .catch((error) => {
+        this.setState({ isLoading: false, error })
+      })
+  }, 400)
+
+  onLabelChange = (event) => {
+    const label = event.target.value
+    if (label.trim() === '') {
+      const input = event.target
+      input.value = ''
+      input.placeholder = 'The request cannot be empty!'
+      return
+    }
+    this.setState({
+      label: event.target.value,
+      movies: [],
+    })
+    this.getMoviesList()
+  }
+
+  clearForm = () => {
+    this.setState({
+      label: '',
+      movies: [],
+    })
+  }
+
+  render() {
+    return (
+      <section className="wrapper">
+        <Input
+          placeholder="Type to search..."
+          value={this.state.label}
+          onChange={this.onLabelChange}
+          suffix={
+            <Tooltip title="Clear the form">
+              <CloseOutlined onClick={this.clearForm} />
+            </Tooltip>
+          }
+        />
+        <Movies
+          movies={this.state.movies}
+          error={this.state.error}
+          isLoading={this.state.isLoading}
+          label={this.state.label}
+          noResults={this.state.noResults}
+        />
+      </section>
+    )
+  }
 }
-
-export default App
--- src/components/movies/movies.js
+++ src/components/movies/movies.js
@@ -1,59 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Space, Spin, Alert } from 'antd'
 import { Offline, Online } from 'react-detect-offline'
 
 import Movie from '../movie/movie'
 import './movies.css'
-import TmdbService from '../../services/tmdb-service'
 
-export default class Movies extends Component {
-  state = {
-    movies: [],
-    isLoading: false,
-    error: null,
-  }
+export default function Movies({ isLoading, movies, error, noResults }) {
+  const moviesList = movies.map((movie) => <Movie movie={movie} key={movie.id} />)
 
-  UNSAFE_componentWillMount() {
-    this.getMoviesList()
-  }
-
-  getMoviesList() {
-    this.setState({ isLoading: true })
-    const moviesService = new TmdbService()
-
-    moviesService
-      .getMovies('https://api.themoviedb.org/3/search/movie?query=return&include_adult=false&language=en-US&page=1')
-      .then((moviesData) => {
-        const moviesList = moviesData.map((movie) => <Movie movie={movie} key={movie.id} />)
-        this.setState({ isLoading: false, movies: moviesList })
-      })
-      .catch((error) => {
-        this.setState({ isLoading: false, error })
-      })
-  }
-
-  render() {
-    const { isLoading, movies, error } = this.state
-
-    return (
-      <>
-        <Online>
-          <section className="movies">
-            {isLoading && (
-              <Space size="middle">
-                <Spin size="large" />
-              </Space>
-            )}
-            {error && <Alert message={error.message} type="info" />}
-            {movies && movies}
-          </section>
-        </Online>
-        <Offline>
-          <section className="movies">
-            <Alert message="No Internet connection" type="info" />
-          </section>
-        </Offline>
-      </>
-    )
-  }
+  return (
+    <>
+      <Online>
+        <section className="movies">
+          {isLoading && (
+            <Space size="middle">
+              <Spin size="large" />
+            </Space>
+          )}
+          {error && <Alert message={error.message} type="info" />}
+          {noResults ? <Alert message="Movies not found" type="info" /> : moviesList}
+        </section>
+      </Online>
+      <Offline>
+        <section className="movies">
+          <Alert message="No Internet connection" type="info" />
+        </section>
+      </Offline>
+    </>
+  )
 }
